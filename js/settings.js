b48ad49dad8721ffb6b318cae9b176289c53fea0
Hari Krishnan <35679989+39754-Hari@users.noreply.github.com>
2018-10-17 18:47:30 +0530

    Add files via upload

---
+++ js/settings.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/* -------------------------------------------------------------------
+Copyright (c) 2017-2017 Hexaware Technologies
+This file is part of the Innovation LAB - Offline Bot.
+------------------------------------------------------------------- */
+//6c7e02249ef94cc68b647881b36f0739
+
+define({
+    accessToken: '58361045745b4b149d7002b4a44709d7',
+    chatServerURL : "https://api.api.ai/v1/",
+    applicationTitle: "Chat Assistant",
+    userTitle : "Me",
+    botTitle : "Opty Assistant",
+    botAvatar : "./avatar/bot-logo-image.png",
+    userAvatar : "avatar/user.png",
+    attachmentUploadURL : "",
+    platform : "facebook",
+    showHeader: true,
+    incompleteTran: ["BillUpdateIntent", "BillUpdateIntent-Yes", "BillUpdate-SwitchPlan", "BillUpdate-Confirm-SwitchPlan"]
+});
