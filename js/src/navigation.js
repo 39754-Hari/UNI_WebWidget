6a7f50a29a32e0144a572cf1bfd9e0ec63d30677
Hari Krishnan <35679989+39754-Hari@users.noreply.github.com>
2018-10-17 18:44:13 +0530

    Add files via upload

---
+++ js/src/navigation.js
@@ -0,0 +1,9 @@
+define(['navigation'], function (navigation) {
+
+    var methods = {};
+    methods.data = {
+        "chatwindow": "Hi",
+        "Scholorship": "Scholorship"
+    }
+    return methods;
+});
